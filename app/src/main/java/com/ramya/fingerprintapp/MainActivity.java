e21e7c0c387c4d6d85e6f2c6115b67866275f724
sai-ramya-N <ramyamrn2002@gmail.com>
2021-04-17 17:01:57 +0530

    update
    app completed successfully

--- app/src/main/java/com/ramya/fingerprintapp/MainActivity.java
+++ app/src/main/java/com/ramya/fingerprintapp/MainActivity.java
@@ -1,14 +1,78 @@
 package com.ramya.fingerprintapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView authStatusTV;
+    private Button authBtn;
+
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authStatusTV = findViewById(R.id.authStatusTV);
+        authBtn = findViewById(R.id.authBtn);
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                String errMsg = "Authentication Error: " + errString;
+                authStatusTV.setText(errMsg);
+                Toast.makeText(MainActivity.this, errMsg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Log.i("onAuthenticationSucceed", String.valueOf(result));
+                String successMsg = "Authentication succeed...!";
+                authStatusTV.setText(successMsg);
+                Toast.makeText(MainActivity.this, successMsg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                String failedMsg = "Authentication failed...!";
+                authStatusTV.setText(failedMsg);
+                Toast.makeText(MainActivity.this, failedMsg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Fingerprint Authentication App")
+                .setSubtitle("Login using fingerprint authentication")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        authBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                biometricPrompt.authenticate(promptInfo);
+
+            }
+        });
     }
 }
