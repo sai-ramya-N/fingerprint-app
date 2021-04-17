9644b9aa871773f00c89463a51aa18d4bb43163c
sai-ramya-N <ramyamrn2002@gmail.com>
2021-04-17 15:44:50 +0530

    initial commit

---
+++ app/src/main/java/com/ramya/fingerprintapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ramya.fingerprintapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
